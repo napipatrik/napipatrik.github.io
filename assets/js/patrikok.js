29d5df9f60328399dfff171db63f4c310b8bfe69
Laszlo Meszaros <laszlo.meszaros@logmein.com>
2019-04-18 10:21:26 +0200

    Update patrikok.js

--- assets/js/patrikok.js
+++ assets/js/patrikok.js
@@ -4,5 +4,7 @@ var patrikok = [
     'Kozma művek - mi szarjuk a pénzt',
     'De ezek fogyatékosak!',
     'Úúú...',
-    'Hát ez végülis működhet ...'
+    'Hát ez végülis működhet ...',
+    'Hát Én mindjárt hazamegyek',
+    'Kézi munka, az ...'
 ];
