3ab1d0f3747449d5dabe9b23711b774cf54dada6
Andras Rutkai <andras.rutkai@logmein.com>
2019-07-04 14:34:53 +0200

    Motd javítások

--- motd.sh
+++ motd.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PATH="$PATH:/usr/games"
+
 if command -v curl >/dev/null 2>&1; then
-  LIST_JS=`curl -s 'https://napipatrik.hu/assets/js/patrikok.js'`
+  LIST_JS=`curl -s -m 5 'https://napipatrik.hu/assets/js/patrikok.js'`
 elif command -v wget >/dev/null 2>&1; then
-  LIST_JS=`wget -qO- 'https://napipatrik.hu/assets/js/patrikok.js'`
+  LIST_JS=`wget -T 5 -qO- 'https://napipatrik.hu/assets/js/patrikok.js'`
 else
   echo "Install either wget or curl!"
   exit 1
@@ -14,6 +16,12 @@ LIST=($(echo "$LIST_JS" | egrep "^\s*'[^']+',?" | sed "s/[',]//g"))
 OFFSET=$(($(($((`date +%Y` - 1970)) * 365  + `date +%j`)) % ${#LIST[@]} ))
 DAILY_PATRIK=${LIST[$OFFSET]}
 
+
 echo -e "  Napi Patrik:"
-echo -e $DAILY_PATRIK | sed -e 's/^[ \t]*//'
+if command -v cowsay >/dev/null 2>&1; then
+  echo -e $DAILY_PATRIK | sed -e 's/^[ \t]*//' | cowsay
+else
+  echo -e $DAILY_PATRIK | sed -e 's/^[ \t]*//'
+  echo
+fi
 
