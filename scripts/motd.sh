0d94def0cbd3302df5a9dc7f48e3b35027b45ff9
Andras Rutkai <andras.rutkai@logmein.com>
2019-09-13 15:03:32 +0200

    Új patrik script, új motd

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -3,18 +3,13 @@
 PATH="$PATH:/usr/games"
 
 if command -v curl >/dev/null 2>&1; then
-  LIST_JS=`curl -s -m 5 'https://napipatrik.hu/assets/js/patrikok.js'`
+  DAILY_PATRIK=`curl -s -m 5 'https://napipatrik.hu/napipatrik'`
 elif command -v wget >/dev/null 2>&1; then
-  LIST_JS=`wget -T 5 -qO- 'https://napipatrik.hu/assets/js/patrikok.js'`
+  DAILY_PATRIK=`wget -T 5 -qO- 'https://napipatrik.hu/napipatrik'`
 else
   echo "Install either wget or curl!"
   exit 1
 fi
-
-IFS=$'\n'
-LIST=($(echo "$LIST_JS" | egrep "^\s*'[^']+',?" | sed "s/[',]//g"))
-OFFSET=$(($(($((`date +%Y` - 1970)) * 365  + `date +%j`)) % ${#LIST[@]} ))
-DAILY_PATRIK=${LIST[$OFFSET]}
 
 
 echo -e "  Napi Patrik:"
