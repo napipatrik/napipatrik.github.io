a2c450e03b127d0b8a87a993f4f4486d870270e8
Andras Rutkai <andras.rutkai@logmein.com>
2019-09-20 15:21:28 +0200

    API upgrade to use S3 and help

--- aws/napipatrik-api-lambda.js
+++ aws/napipatrik-api-lambda.js
@@ -1,54 +1,68 @@
 const url = require('url');
 const qs = require('querystring');
 
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = (event, context, callback) => {
-    let id = qs.parse(event.body)['text'];
+    var id = qs.parse(event.body)['text'];
 
-    let url;
-    if (id) {
-        console.log("ID: ", id);
-
-        url = "https://napipatrik.hu/assets/js/patrikok.js";
-    } else {
-        url = "https://napipatrik.hu/napipatrik";
-    }
-
-    
-    require('https').get(url, function(resp) {
-        let data = '';
-        let body = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            if (id) {
-                eval(data);
-                id = Number(id) == NaN ? 0 : Number(id);
-                body = patrikok[id] === undefined ? patrikok[0] : patrikok[id];
-            } else {
-                body = data.trim();
-            }
-
-            callback(null, {
-                "statusCode": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                body: body
-            });
-        });
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
+    if (id.trim() === "help") {
         callback(null, {
-            "statusCode": 500,
+            "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(e)
+            body: JSON.stringify({
+                "response_type": "ephemeral",
+                "text": "Használat: `/napipatrik [id]`\nahol az opcionális id az idézet száma (permalink id)"
+            })
+        });
+        return;
+    }
+
+    let file;
+    if (id) {
+        console.log("ID: ", id);
+
+        file = "assets/js/patrikok.js";
+    } else {
+        file = "napipatrik";
+    }
+
+    var params = { Bucket: 'napipatrik', Key: file };
+    s3.getObject(params, function(err, data) {
+        if (err) {
+            console.log("Got error: ");
+            console.log(err);
+            callback(null, {
+                "statusCode": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(err)
+            });
+            return;
+        }
+
+        let body = '';
+        if (id) {
+            eval(data.Body.toString());
+            id = Number(id) == NaN ? 0 : Number(id);
+            body = patrikok[id] === undefined ? patrikok[0] : patrikok[id];
+        } else {
+            body = data.Body.toString().trim();
+        }
+
+        callback(null, {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "response_type": "in_channel",
+                "text": body
+            })
         });
     });
 };
