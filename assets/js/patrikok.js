cbbe2edf1fe5bb566c7fa0b4d7f6cc5805518fed
Andras Rutkai <andras.rutkai@logmein.com>
2019-10-18 10:39:39 +0200

    Még Patrik

--- assets/js/patrikok.js
+++ assets/js/patrikok.js
@@ -195,7 +195,8 @@ var patrikok = [
     'Nem ölöm meg, csak összehajtogatom, hogy beleférjen a táskájába',
     'Laci, a lófasz nem mennek ki, azok bejönnek',
     '- Dolgozol Patrik?\n- Tudom undorító így szerdán, de hát ez van',
-    'Menjünk haza, hátha holnap jobb lesz...'
+    'Menjünk haza, hátha holnap jobb lesz...',
+    'A faszerdőben a cserjéket is vinni kell, tudom András, te az ős-tölgyeket szereted'
 ];
 
 if (typeof exports !== 'undefined') {
