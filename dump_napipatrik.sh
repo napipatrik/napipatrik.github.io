1bebbba386b44c7e852dc381e52c4206f226cb56
Andras Rutkai <riskawarrior@live.com>
2019-11-08 13:24:00 +0100

    Még patrikok Permalink fix

--- dump_napipatrik.sh
+++ dump_napipatrik.sh
@@ -6,6 +6,7 @@ if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
 fi
 
 node napipatrik.js > napipatrik
+node napipatrik.js --index > napipatrik.id
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
@@ -14,6 +15,7 @@ git config --global push.default simple
 git remote set-url origin https://${GH_TOKEN}@github.com/napipatrik/napipatrik.github.io.git > /dev/null 2>&1
 git checkout master
 git add $TRAVIS_BUILD_DIR/napipatrik
+git add $TRAVIS_BUILD_DIR/napipatrik.id
 git commit -m "Rotate napipatrik"
 git push
 
--- napipatrik.js
+++ napipatrik.js
@@ -1,7 +1,13 @@
 const patrikFn = require('./assets/js/functions');
 const patrikok = require('./assets/js/patrikok');
 
+const args = process.argv.slice(2);
+
 
 let index = patrikFn.getDefaultOffset() % patrikok.length;
-console.log(patrikok[index]);
 
+if (args.length && args[0] === '--index') {
+  console.log(index);
+} else {
+  console.log(patrikok[index]);
+}
