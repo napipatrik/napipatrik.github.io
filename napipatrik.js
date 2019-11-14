dcda0d858e338bc9fdefaaba1b9a1ed103cb710c
Andras Rutkai <andras.rutkai@logmein.com>
2019-11-14 11:04:39 +0100

    Képgenerálás

--- napipatrik.js
+++ napipatrik.js
@@ -5,9 +5,24 @@ const args = process.argv.slice(2);
 
 
 let index = patrikFn.getDefaultOffset() % patrikok.length;
+if (args.length && args[0] === '--image') {
+  (async function () {
+    const Jimp = require('jimp');
+    const request = require('request-promise-native');
+    const fs = require('fs');
 
-if (args.length && args[0] === '--index') {
+    const imageOriginal = await request({url: 'https://source.unsplash.com/600x600/?nature,calm', encoding: null});
+    fs.writeFileSync('./napipatrik.jpg', imageOriginal);
+    const image = await Jimp.read('./napipatrik.jpg');
+    const shadow = await Jimp.read('./shadow.png');
+    await image.blit(shadow, 0, 0);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    await image.print(font, 100, 250, {text: patrikok[index], alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE}, 400);
+    await image.write('napipatrik.jpg');
+  })();
+} else if (args.length && args[0] === '--index') {
   console.log(index);
 } else {
   console.log(patrikok[index]);
 }
+
