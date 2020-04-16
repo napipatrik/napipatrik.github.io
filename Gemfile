e168db8975628527dff9698ce2c788227793ee01
Andras Rutkai <riskawarrior@live.com>
2020-04-16 19:41:46 +0200

    Search box!!!4!4

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem "minima", "~> 2.0"
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
-  gem "github-pages", "~> 202"
+  gem "github-pages", "~> 204"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,9 +2,47 @@ if (typeof moment === 'undefined') {
     var moment = require('./moment.min');
 }
 
+function loadSearch() {
+	var list = document.getElementById("tutilist-inner");
+	for (var i in patrikok) {
+		var a = document.createElement('a');
+		a.href = '#' + i;
+		a.innerText = patrikok[i].replace('\n', '<br/>');
+		a.onclick = showPatrikSearchFactory(i);
+		list.appendChild(a);
+	}
+}
+
+function showSearch() {
+	document.getElementById("tutilist").classList.toggle("show");
+}
+
+function filterTuti() {
+    var input = document.getElementById("tuti-search");
+    var filter = input.value.toUpperCase();
+    var a = document.getElementById("tutilist").getElementsByTagName("a");
+    for (var i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function showPatrikSearchFactory(i) {
+	return function () {
+	    showPatrik(i);
+	    window.location.hash = i;
+	    document.getElementById("tutilist").classList.toggle("show");
+	    return false;
+	}
+}
+
 function showPatrik(i) {
     let index = i % patrikok.length;
-
+console.log(i);
     document.getElementById('napituti').innerHTML = patrikok[index].replace('\n', '<br/>');
     document.getElementById('permalink').href = window.location.href.replace(window.location.hash, '') + '#' + index;
 }
