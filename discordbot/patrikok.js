e3f96e470c7f5679603bdd5d93f5bae73f12f266
Andras Rutkai <riskawarrior@live.com>
2021-03-18 14:58:22 +0100

    Randomize order in search

--- discordbot/patrikok.js
+++ discordbot/patrikok.js
@@ -41,10 +41,29 @@ exports.find = function (keywords) {
     }
 
     if (filteredList.length) {
-      return filteredList[0];
+      return shuffle(filteredList)[0];
     }
   } catch (e) {
   }
 
   return '';
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
