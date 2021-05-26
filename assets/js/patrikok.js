c68475d9e17d6c516198ceccdfe56b233ff0755d
Andras Rutkai <riskawarrior@live.com>
2021-05-26 15:41:11 +0200

    Még Patrik

--- assets/js/patrikok.js
+++ assets/js/patrikok.js
@@ -425,6 +425,7 @@ var patrikok = [
     'Csaba: Miért nincs hammeryolo emoji?\nPatrik: mert az több, mint egy emoji: egy életérzés',
     'Faszom, hogy legyek motivált erdész? Ha egy kicsit is elmosolyodik a szám, abban a pillanatban betalál egy körszakáll és azon a pici résen egyből tarkóig szalad.',
     'A pénz a lényeg. A körszakállakat meg bírod. Összeszorítod a fogaid meg a púzód, aztán meg kisbaltázol az erdőben, letelik a 8 óra, kisbalta le, ánusz elernyed és örömkÖdsz.',
+    'Tudjuk Csaba, tömött erdész, boldog erdész.',
 ];
 var tutik = patrikok;
 
