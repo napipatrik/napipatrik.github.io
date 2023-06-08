1089026ad2bf4329e3ba4ed81ecc0c925cb71c56
András Rutkai <riskawarrior@live.com>
2023-06-08 22:47:53 +0200

    Don't dump on any change

--- dump_napituti.sh
+++ dump_napituti.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-node napituti.js > napipatrik
-node napituti.js --index > napipatrik.id
-
-# Check if there is a change since last run
-if [ $(cat napipatrik.id) = $(curl -s https://napipatrik.hu/napipatrik.id) ] || [ "$GITHUB_EVENT_NAME" != "schedule" ]; then
+# Change anything only on cron schedule
+if [ "$GITHUB_EVENT_NAME" != "schedule" ]; then
   echo "Daily quote is the same, using existing dumps"
+  curl -s https://napipatrik.hu/napipatrik --output napipatrik
+  curl -s https://napipatrik.hu/napipatrik.id --output napipatrik.id
   curl -s https://napipatrik.hu/napipatrik.jpg --output napipatrik.jpg
   curl -s https://napipatrik.hu/rss.xml --output rss.xml
   exit 0
 fi
 
 echo "Dumping daily quote assets..."
+node napituti.js > napipatrik
+node napituti.js --index > napipatrik.id
+
 npm i node-fetch@2.6.6 jimp
 node napituti.js --image
 
