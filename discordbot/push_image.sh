191b85d92542ca6a92cb644d901b5e5faaac3b6b
András Rutkai <riskawarrior@live.com>
2023-12-28 12:44:14 +0100

    Add build to discordbot

--- discordbot/push_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t rutkai/napipatrik-discordbot:latest .
-docker push rutkai/napipatrik-discordbot:latest
-
-# docker run --pull always --name napipatrik-discordbot -e DISCORD_BOT_TOKEN=very-top-secret-token --restart unless-stopped discordbot
