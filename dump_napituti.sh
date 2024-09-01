b254d7236071f4786a7cdb3d7e513e798e96dde0
András Rutkai <riskawarrior@live.com>
2024-09-01 07:52:31 +0200

    Fix npm package versions

--- dump_napituti.sh
+++ dump_napituti.sh
@@ -3,15 +3,16 @@
 set -e
 
 # Change anything only on cron schedule
-if [ "$GITHUB_EVENT_NAME" != "schedule" ]; then
-  echo "Daily quote is the same, using existing dumps"
-  curl -s https://napipatrik.hu/napipatrik --output napipatrik
-  curl -s https://napipatrik.hu/napipatrik.id --output napipatrik.id
-  curl -s https://napipatrik.hu/napipatrik.jpg --output napipatrik.jpg
-  curl -s https://napipatrik.hu/rss.xml --output rss.xml
-  exit 0
-fi
+#if [ "$GITHUB_EVENT_NAME" != "schedule" ]; then
+#  echo "Daily quote is the same, using existing dumps"
+#  curl -s https://napipatrik.hu/napipatrik --output napipatrik
+#  curl -s https://napipatrik.hu/napipatrik.id --output napipatrik.id
+#  curl -s https://napipatrik.hu/napipatrik.jpg --output napipatrik.jpg
+#  curl -s https://napipatrik.hu/rss.xml --output rss.xml
+#  exit 0
+#fi
 
+npm ci
 echo "Dumping daily quote assets..."
 node napituti.js > napipatrik
 node napituti.js --index > napipatrik.id
